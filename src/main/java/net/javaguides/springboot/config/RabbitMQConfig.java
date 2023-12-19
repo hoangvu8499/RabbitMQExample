dd362a4b0e173f1d0dc00e783b8823bb31d257d6
TimoTon84 <tton@equabli.com>
2023-12-19 07:20:37 +0700

    rabbit mq example

---
+++ src/main/java/net/javaguides/springboot/config/RabbitMQConfig.java
@@ -0,0 +1,46 @@
+package net.javaguides.springboot.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    // Spring Bean for rabbitmq queue
+    @Bean
+    public Queue queue() {
+        return new Queue(queue);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    // Binding between queue and exchange using routing key
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+
+    //ConnectionFactory
+    //RabbitTemplate
+    //RabbitAdmin
+    //--> 3 cai Bean nay dc sping config san roi nen kh can lam nk
+
+}
---
+++ src/main/java/net/javaguides/springboot/controller/MessageController.java
@@ -0,0 +1,23 @@
+package net.javaguides.springboot.controller;
+
+import net.javaguides.springboot.publisher.RabbitMQProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+    @Autowired
+    private RabbitMQProducer rabbitMQProducer;
+
+    //http://localhost:8080/api/v1/publish?message=hello
+    @GetMapping("/publish")
+    public ResponseEntity<String> sendMessage(@RequestParam("message") String message) {
+        rabbitMQProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to RabbhitMQ.....");
+    }
+}
